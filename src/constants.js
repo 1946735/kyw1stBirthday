c1cc191d0c3805aefcb50eadcd7216bf3697dbfe
1946735 <1946735@naver.com>
2023-09-23 00:41:49 +0900

    index.html update for 카카오톡 공유하기

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const BASEURL = `${process.env.REACT_APP_BASEURL}`
-// export const BASEURL = `http://192.168.0.103:8001/`
-// export const BASEURL = `https://kkkhg.site/`
+// export const BASEURL = `http://192.168.0.9:3000/`
+// export const BASEURL = `https://github.com/1946735`
 export const APIPATH = {
     //WEDDING: 'wedding/'
     BIRTHDAY: '1stbirthday/'
