96c7390051b3a1242ef30dc604cdd1fec9e0a575
1946735 <1946735@naver.com>
2023-10-21 23:36:11 +0900

    release rev 06

--- src/component/adress/Map.js
+++ src/component/adress/Map.js
@@ -12,7 +12,12 @@ const Map = () => {
     useEffect(() => {
         if(cnt < 1) setCnt(prev => ++prev);
         else if(cnt == 1) {
- 
+
+            const mainImg = new Image();
+            mainImg.src = require('../assets/images/take_hotel_1.jpg');
+            
+            mainImg.onload = () => setIsImgReady(true);
+
             const container = document.querySelector('.map');
             const options = {
                 center: new window.kakao.maps.LatLng(lat, lon),
@@ -71,6 +76,12 @@ const Map = () => {
                         <img className="map_icon" src={require('../../assets/images/t_map.png')}/>
                     </Button>
                     <div>티맵</div>
+                </div>
+                <div>
+                <AnimatedWrap>                        
+                        <MainImg />
+                        <div>참고사진</div>
+                </AnimatedWrap>
                 </div>
             </div>
         </div>
